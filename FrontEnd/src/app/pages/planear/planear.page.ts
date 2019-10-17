157acba7fd2e7bd4fff72095bc1b2eb72b6ea4ae
Jorge Hernan Arcila Alzate <jorarcil@bancolombia.com.co>
2019-10-17 16:06:57 -0500

    consumo servicio rutas

--- FrontEnd/src/app/pages/planear/planear.page.ts
+++ FrontEnd/src/app/pages/planear/planear.page.ts
@@ -8,17 +8,13 @@ import { DatosRuta } from '../../models/datosRuta';
   styleUrls: ['./planear.page.scss'],
 })
 export class PlanearPage implements OnInit {
-  datosRutas: DatosRuta[] = [];
+  datosRutas: any;
   constructor(private verRutaService: VerRutasServices) { }
 
   ngOnInit() {
-    this.verRutas();
-    console.log(this.datosRutas);
-  }
-
-  verRutas() {
     this.verRutaService.verRutasPorPersona('1').subscribe((resp) => {
       this.datosRutas = resp;
+      console.log('--------->', this.datosRutas);
     });
   }
 }
